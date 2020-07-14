e18c24d048f8b7eeb5efe6a8b42df5e27558e19c
theBar10 <37342344+theBar10@users.noreply.github.com>
2020-07-13 22:59:38 -0500

    directory change, archive, progress notes
    1.) changed the final save directory to a different folder so this script could be ran again without duplicating the data
    
    2.) made a second copy of the merge data with a dynamic name with the current date for historically reference
    
    3.) added a series of print functions that shows the progress and directory locations. I partly did this for trouble shooting, but mostly did it because I was bored and wanted to see it doing something.

--- code.py
+++ code.py
@@ -1,13 +1,31 @@
 import os
 import glob
 import pandas as pd
+import datetime 
 
-#print(os.getcwd())
+print("starting...")
+print("current directory:")
+print(os.getcwd())
+# Returns the current local date 
+current_time = datetime.datetime.now()
+year = current_time.year
+month = current_time.month
+day = current_time.day
+
+current_time = str("{}_{}_{}".format(year, month, day))
 
 os.chdir(r"C:\Users\tbarten\Desktop\Projects\Cost_Productivity\PyRun")
+print("grabbing data from directory:")
+print(os.getcwd())
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 #combine all files in the list
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
 #export to csv
+os.chdir(r"C:\Users\tbarten\Desktop\Projects\Cost_Productivity\PyRun\Master File")
+print("saving master file in directory:")
+print(os.getcwd())
 combined_csv.to_csv( "master_csv.csv", index=False, encoding='utf-8-sig')
+combined_csv.to_csv( "archive_master_csv_{}.csv".format(current_time), index=False, encoding='utf-8-sig')
+
+print("finished at {}".format(current_time))
